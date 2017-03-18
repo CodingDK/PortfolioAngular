11cf4978ad71839fef19109c597c5feed8a913f4
CodingDK <CodingDK@users.noreply.github.com>
2017-03-18 11:59:08 +0100

    added modal for showing images on portfolio

--- PortfolioAngular/ClientApp/app/portfolio/portfolio-detail/portfolio-detail.component.ts
+++ PortfolioAngular/ClientApp/app/portfolio/portfolio-detail/portfolio-detail.component.ts
@@ -12,7 +12,7 @@ import { PortfolioService } from '../shared/portfolio.service';
 export class PortfolioDetailComponent implements OnInit {
 
     public item: PortfolioItem = new PortfolioItem();
-    public defaultImg: string = "";
+    //public defaultImg: string = "";
 
     constructor(private route: ActivatedRoute, private router: Router, private service: PortfolioService) {
     }
@@ -22,31 +22,55 @@ export class PortfolioDetailComponent implements OnInit {
             .switchMap((params: Params) => this.service.getItem(params['slug']))
             .subscribe((item: PortfolioItem) => {
                 this.item = item;
-                this.defaultImg = this.service.getDefaultImgSrc(item);
             });
     }
     
-    getImgSrc(imgName) : string {
-        return this.service.getImgSrc(imgName);
-    }
-
-    getMoreImageUrls(): string[] {
+    getImgSrc(index): string {
         if (this.item == null || this.item.images == null) return;
-        let arr = [];
-        
-        this.item.images.forEach((value, index) => {
-            let src = this.getImgSrc(value);
-            if (src === this.defaultImg) return;
-            arr.push(this.getImgSrc(value));
-        })
-        return arr;
-    }
 
-    changeDefaultImg(imgUrl: string): void {
-        this.defaultImg = imgUrl;
+        return this.service.getImgSrc(this.item.images[index]);
     }
 
     showMoreImages(): boolean {
         return this.item != null && this.item.images != null && this.item.images.length > 1;
+    }
+
+    //gallery stuff
+    public isOpen: boolean = false;
+    public currentIndex: number = 0;
+
+    openGallery(index: number): void {
+        this.isOpen = true;
+        this.setCurrentIndex(index);
+        this.setClassOnBody(true);
+    }
+
+    closeGallery(): void {
+        this.isOpen = false;
+        this.setClassOnBody(false);
+    }
+
+    private setClassOnBody(add:boolean): void {
+        if (typeof document === "undefined") return;
+        let list = document.querySelector('body').classList;
+        if (add) {
+            list.add('modal-open');
+        } else {
+            list.remove('modal-open');
+        }
+    }
+
+    moveSlide(forward: boolean): void {
+        if (this.currentIndex === 0 && !forward) {
+            this.currentIndex = this.item.images.length-1;
+        } else if (forward && this.currentIndex === this.item.images.length-1) {
+            this.currentIndex = 0;
+        } else {
+            this.currentIndex += (forward) ? 1 : -1;
+        }
+    }
+
+    setCurrentIndex(index: number): void {
+        this.currentIndex = index;
     }
 }
